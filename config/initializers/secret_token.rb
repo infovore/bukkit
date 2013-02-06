e51028ffe2bbd54836153d1e7330fc9fb95de864
Tom Armitage <tom@infovore.org>
2013-02-06 16:05:21 +0000

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bukkit::Application.config.secret_token = '7b0fce3be09a42795aea1e12eb63f65a5f6602b2f3011aacd222ce51440959ed8e99dc7f882d01f300d2b567bc909695b48f0b1ef692a15e40c32ab6d42e0fcc'

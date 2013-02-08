4e50804da08cffadbf1f894e7fe96b2ba1aa4476
Tom Armitage <tom@infovore.org>
2013-02-08 17:49:02 +0000

    Bloody assets.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,8 +45,8 @@ Bukkit::Application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
    config.action_controller.asset_host = "http://ihasabukkit.herokuapp.com"
 
-  # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+   #Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
+  config.assets.precompile += %w( lp.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false

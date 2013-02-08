2b0574188a3e996c3dec7b9e9dc4f4d4be7cbd68
Tom Armitage <tom@infovore.org>
2013-02-08 16:45:53 +0000

    Better sample text.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,7 +21,7 @@ class ItemsController < ApplicationController
 
   def sample
     @items = [Item.new(:content => "Pick up milk"),
-              Item.new(:content => "ring the doctor"),
+              Item.new(:content => "ring the doctor about the horrible rash on my thigh"),
               Item.new(:content => "25 push-ups today!")]
     render :index
   end

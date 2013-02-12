6348135064a4978ae538f1d5b5a70fe8e247cee7
Tom Armitage <tom@infovore.org>
2013-02-12 16:19:49 +0000

    Putting this back how it was.

--- config/application.rb
+++ config/application.rb
@@ -66,7 +66,7 @@ module Bukkit
     config.assets.version = '1.0'
 
     # Stop precompile on heroku loading DB
-    config.assets.initialize_on_precompile = false
+    #config.assets.initialize_on_precompile = false
     
   end
 end
